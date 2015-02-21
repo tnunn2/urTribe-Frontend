b8ed0e56a658179c36cbda070a15880059cddf43
Thomas Nunnery <tnunn2@gmail.com>
2015-02-20 21:01:43 -0600

    Removing temp project

--- code/my-app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Thomas\se491\code\my-app\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Thomas\se491\code\my-app\platforms\android\CordovaLib\AndroidManifest.xml \
--- code/my-app/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Thomas\se491\code\my-app\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Thomas\se491\code\my-app\platforms\android\AndroidManifest.xml \
