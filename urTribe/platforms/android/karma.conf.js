c67762e9e75b95e766844da8d987dd446bb80021
tnunn2 <tnunn2@gmail.com>
2015-02-22 16:33:00 -0600

    Update karma.conf.js

--- urTribe/platforms/android/karma.conf.js
+++ urTribe/platforms/android/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'C:/Users/Thomas/se491/urTribe/www/js/',
+    basePath: '../../www/js/',
 
 
     // frameworks to use
