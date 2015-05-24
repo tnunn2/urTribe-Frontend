5307e3b5b1498fcbdc466da8b44ff911740bcaea
Blair Douglass <blair.douglass@digitas.com>
2015-05-24 14:18:34 -0500

    Added event binding to dashboard and ability to select the user using the application

--- urTribe/www/js/controllers/mainController.js
+++ urTribe/www/js/controllers/mainController.js
@@ -6,7 +6,23 @@ urtribeControllers.controller('MainController', function($scope, $ionicModal, $i
     $ionicHistory.goBack();
   };
 
+  $scope.Users = [
+        {name: 'Jimmy James', token:"1b37a029-7784-4417-9eac-144fe5088439"},
+        {name: 'Ashly Thomas', token:"3cc37867-2a0d-43df-b03d-ddc03dc869dc"}
+        ];
+
+  $scope.userSelected = $scope.Users[0];
+
+  $scope.setUser = function(user) {
+    $scope.userSelected = user;
+  }
+
+  $scope.selectedUser = function(user) {
+    return $scope.userSelected === user;
+  }
+
   MessageService.initialize();
   //TODO - create API handler singleton here - create class and instantiate
+
 
 })
--- urTribe/www/js/models/event.js
+++ urTribe/www/js/models/event.js
@@ -3,7 +3,7 @@ urtribeModels.factory('Event', function () {
       /**
      * Constructor, with class name
      */
-    function Event(id, name, active, time, location, street1, street2, city, state, zip) {
+    function Event(id, name, active, time, location, street1, street2, city, state, zip, description) {
       // Public properties, assigned to the instance ('this')
       this.ID = id,
       this.Name = name,
@@ -15,6 +15,7 @@ urtribeModels.factory('Event', function () {
       this.City = city,
       this.State = state,
       this.Zip = zip,
+      this.description = description,
       this.AttendanceStatus = "Attending";
     }
 
@@ -25,12 +26,25 @@ urtribeModels.factory('Event', function () {
       return this.name + ':' + "here is what this event is about";
     };
 
+    Event.prototype.getMapURL = function () {
+      if(this.Street2 !=null)
+      {
+        return "http://maps.google.com/?q=" + this.Street1 + " " + this.Street2 + "," + this.City  + "," + this.State + "," + this.Zip;
+      }
+      else {
+        return "http://maps.google.com/?q=" + this.Street1  + "," + this.City  + "," + this.State + "," + this.Zip;
+      }
+
+    }
+
 
       /**
      * Static method, assigned to class
      * Instance ('this') is not available in static context
      */
     Event.build = function (data) {
+      //format Time
+
       return new Event(
         data.ID,
         data.Name,
@@ -41,8 +55,13 @@ urtribeModels.factory('Event', function () {
         data.Street2,
         data.City,
         data.State,
-        data.Zip
+        data.Zip,
+        data.Description
       );
+
+      //get attendance Status
+
+      //get people attending
     };
 
     /**
